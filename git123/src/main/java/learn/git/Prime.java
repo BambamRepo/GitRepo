57512607006fd2d40b63a28bfc0dc678174d229c
Bambam <mahasethbambam@gmail.com>
2023-02-12 20:35:00 +0530

    team B has written code

--- git123/src/main/java/learn/git/Prime.java
+++ git123/src/main/java/learn/git/Prime.java
@@ -3,12 +3,23 @@ package learn.git;
 public class Prime {
 
 	public static void main(String[] args) {
-		int n=7;
+		int n=7,c=0;
 		for(int i=1;i<=n;i++)
 		{
 			//this is done by team B
 		  	//this is done by team B
 			//Now team A make some progress
+			//team B started writing code
+			if(n%i==0)
+			{
+				c++;
+			}
+		}
+		if(c==2)
+		{
+			System.out.println("Prime no");
+		}else {
+			System.out.println("Not a Prime no");
 		}
 
 	}
